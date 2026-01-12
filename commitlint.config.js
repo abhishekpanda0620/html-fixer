8c6d06a186fbd91b1067acccc45c948e4be81db6
Abhishek Panda <abhishek.panda0620@gmail.com>
2026-01-12 23:37:53 +0530

    chore: allow semantic-release commits to bypass commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,5 @@
 export default {
     extends: ['@commitlint/config-conventional'],
+    // Ignore semantic-release generated commits
+    ignores: [(commit) => commit.includes('chore(release)')],
 };
